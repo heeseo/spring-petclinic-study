4ac6c7caa5eff14872bcde52c15d929b7856d3fb
Heekwon <seoheekwon@gmail.com>
2024-01-17 22:13:04 +0000

    feat: Add findByName method to OwnerRepository

--- src/main/java/study/petclinic/repository/OwnerRepository.java
+++ src/main/java/study/petclinic/repository/OwnerRepository.java
@@ -1,6 +1,8 @@
 package study.petclinic.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.petclinic.domain.Owner;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
     List<Owner> findByFirstName(String firstName);
+
+    @Query("select o from Owner o where o.firstName = :name or o.lastName = :name")
+    List<Owner> findByName(@Param("name") String name);
 }
