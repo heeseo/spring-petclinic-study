8c38e96fca8b97e41007e6d351fb884e39ceab86
Heekwon <seoheekwon@gmail.com>
2024-01-17 00:16:14 +0000

    test: Write tests for OwnerRepository and PetRepository

---
+++ src/test/java/study/petclinic/repository/OwnerRepositoryTest.java
@@ -0,0 +1,61 @@
+package study.petclinic.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.petclinic.domain.Address;
+import study.petclinic.domain.Owner;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class OwnerRepositoryTest {
+
+    @Autowired OwnerRepository ownerRepository;
+
+    @Test
+    public void saveAndFindTest() {
+        Address address = new Address("city", "street", "myzip");
+        Owner owner = new Owner("Fu", "Bao", address, "1112111");
+
+        ownerRepository.save(owner);
+
+        Owner retrievedOwner = ownerRepository.findById(owner.getId()).orElse(null);
+
+        assertThat(retrievedOwner).isNotNull();
+        assertThat(retrievedOwner.getFirstName()).isEqualTo(owner.getFirstName());
+        assertThat(retrievedOwner.getLastName()).isEqualTo(owner.getLastName());
+        assertThat(retrievedOwner).isEqualTo(owner);
+
+        ownerRepository.delete(owner);
+
+        Owner deletedOwner = ownerRepository.findById(owner.getId()).orElse(null);
+
+        assertThat(deletedOwner).isNull();
+    }
+
+    @Test
+    public void findByFirstNameTest() {
+        Address address = new Address("city", "street", "myzip");
+        Owner owner = new Owner("Fu", "Bao", address, "1112111");
+
+        ownerRepository.save(owner);
+
+        List<Owner> owners = ownerRepository.findByFirstName(owner.getFirstName());
+
+        assertThat(owners).isNotEmpty();
+        assertThat(owners.get(0).getFirstName()).isEqualTo(owner.getFirstName());
+        assertThat(owners.get(0).getLastName()).isEqualTo(owner.getLastName());
+        assertThat(owners.get(0)).isEqualTo(owner);
+
+        List<Owner> ownersEmpty = ownerRepository.findByFirstName("NONAME");
+
+        assertThat(ownersEmpty).isEmpty();
+    }
+
+}
