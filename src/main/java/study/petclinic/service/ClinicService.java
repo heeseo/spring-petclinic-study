7801239a02563a6c1256b746e69a1657be58f4e0
Heekwon <seoheekwon@gmail.com>
2024-01-20 21:46:26 +0000

    feat: Add ClinicService

---
+++ src/main/java/study/petclinic/service/ClinicService.java
@@ -0,0 +1,42 @@
+package study.petclinic.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.petclinic.domain.Pet;
+import study.petclinic.domain.Visit;
+import study.petclinic.exception.VisitNotFoundException;
+import study.petclinic.repository.VisitRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ClinicService {
+
+    private final OwnerService ownerService;
+    private final PetService petService;
+    private final VisitRepository visitRepository;
+
+
+    public Long addVisit(Long petId, LocalDateTime date, String description) {
+        Pet pet = petService.findPet(petId);
+        Visit visit = new Visit(pet, date, description);
+        visitRepository.save(visit);
+        return visit.getId();
+    }
+
+    public void editVisit(Long id, LocalDateTime date, String description) {
+        Visit visit = visitRepository.findById(id).orElseThrow(() -> new VisitNotFoundException("Visit not found with id: " + id));
+        visit.updateVisit(date, description);
+    }
+
+    public List<Visit> findAllVisits() {
+        return visitRepository.findAll();
+    }
+
+}
+
+
